071cd1337b46353e7b02a579ed9dcf5c565cbc04
Daryoush Ghoreishi <69098394+DaryoushShah@users.noreply.github.com>
2022-08-13 22:12:53 -0700

    Add navbar

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
+/* createNavbar() */
+const createNavbar = () => {
+  const navbar = document.createElement('div');
+  navbar.classList.add('navbar');
+
+  const title = document.createElement('p');
+  title.classList.add('title');
+  title.textContent = 'Random Stock';
+
+  navbar.appendChild(title);
+
+  return navbar;
+}
+
 /* Append container to body  */
 const container = document.createElement('div');
 container.classList.add('container');
 
 document.body.appendChild(container);
+
+/* Append Navbar to container */
+container.appendChild(createNavbar());
