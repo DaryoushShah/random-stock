f28016b283ee294d5a5c101f2ebb84094f2925e3
Daryoush Ghoreishi <69098394+DaryoushShah@users.noreply.github.com>
2022-08-13 22:19:50 -0700

    Add footer

--- js/app.js
+++ js/app.js
@@ -12,11 +12,30 @@ const createNavbar = () => {
   return navbar;
 }
 
+/* createFooter() */
+const createFooter = () => {
+  const footer = document.createElement('div');
+  footer.classList.add('footer');
+
+  const disclaimer = document.createElement('p');
+  disclaimer.classList.add('text');
+  disclaimer.textContent = 'This program exists for exploratory purposes '
+    + 'only and DOES NOT constitute financial advice. You as the user '
+    + 'take full liability for any trades or actions '
+    + 'taken as a result of using this program.';
+
+  footer.appendChild(disclaimer);
+  return footer;
+}
+
 /* Append container to body  */
 const container = document.createElement('div');
 container.classList.add('container');
 
 document.body.appendChild(container);
 
-/* Append Navbar to container */
+/* Append navbar to container */
 container.appendChild(createNavbar());
+
+/* Append footer to container */
+container.appendChild(createFooter());
